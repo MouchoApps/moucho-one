863b0d0caee2de1fbef905d096db414db81713f8
Vreixo Gonzalez <vreixo@gmail.com>
2017-12-04 19:29:57 +0100

    Reformat code

--- src/main/java/com/mouchoapps/mouchoone/BackendHelloWorldServiceApplication.java
+++ src/main/java/com/mouchoapps/mouchoone/BackendHelloWorldServiceApplication.java
@@ -3,7 +3,6 @@ package com.mouchoapps.mouchoone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(value = {"com.mouchoapps.mouchoone"})
--- src/main/java/com/mouchoapps/mouchoone/web/HelloWorldController.java
+++ src/main/java/com/mouchoapps/mouchoone/web/HelloWorldController.java
@@ -2,7 +2,6 @@ package com.mouchoapps.mouchoone.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
